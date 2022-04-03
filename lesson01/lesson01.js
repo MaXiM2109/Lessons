45c8b13c15dbbe6d0944130dfcdf1474cf0e1765
MaXiM2109 <96611909+MaXiM2109@users.noreply.github.com>
2022-04-03 20:59:16 +0400

    lesson01

---
+++ lesson01/lesson01.js
@@ -0,0 +1,9 @@
+let money = null;
+let income = null;
+let addExpenses  = null;
+let deposit = null;
+let mission = null;
+let period = null;
+alert("что вам надо?");
+let a = "Вот тут будет текст";
+console.log(a); 
